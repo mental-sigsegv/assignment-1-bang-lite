9ea72c2228ccfc95bcc18237ce120a53ba1432d4
Martin Klacik <klacikmartin@protonmail.com>
2023-03-24 12:58:21 +0100

    reworked CatBalou #2

--- src/main/java/sk/stuba/fei/uim/oop/cards/CatBalou.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/CatBalou.java
@@ -10,57 +10,65 @@ public class CatBalou extends Card {
     public CatBalou(CardDeck cardDeck) {
         super(CARD_NAME, cardDeck);
     }
+    private ArrayList<Player> canTarget;
 
     @Override
     public boolean canPlay(Player player) {
-        return true;
+        canTarget = new ArrayList<>();
+
+        for (Player target : cardDeck.players) {
+            if (target != player && target.isAlive()) {
+                if ((target.getActiveCards().size() + target.getCards().size()) > 0) {
+                    canTarget.add(target);
+                }
+            }
+        }
+
+        return canTarget.size() > 0;
     }
     @Override
     public void playCard(Player player, ArrayList<Player> players) {
         super.playCard(player, players);
 
-        ArrayList<Player> targets = new ArrayList<>();
-        for (Player target : players) {
-            if (target != player) {
-                targets.add(target);
-            }
-        }
-
-        Player target = player.chooseTarget(targets);
+        Player target = player.chooseTarget(canTarget);
 
         int typeOfDeck = -1;
+        int cardsSize = target.getCards().size();
+        int activeCardsSize = target.getActiveCards().size();
+        Card selectCard;
 
-        while (typeOfDeck < 1 || typeOfDeck > 2) {
-            typeOfDeck = ZKlavesnice.readInt("1. Cards on hand\n2. Cards on table");
-            if (typeOfDeck < 1 || typeOfDeck > 2) {
-                System.out.println(" !!! Try again! !!!");
+        if (cardsSize == 0) {
+            // Player doesn't have cards on hand
+            System.out.println("--- " + target.getName() + " doesn't have any card on hand. Active card will be removed. ---");
+            selectCard = target.getActiveCards().get((int) (Math.random() * activeCardsSize-1));
+            target.getActiveCards().remove(selectCard);
+        } else if (activeCardsSize == 0) {
+            // Player doesn't have any active cards (prison, barrel, dynamite)
+            System.out.println("--- " + target.getName() + " doesn't have any active card. Card on hand will be removed. ---");
+            selectCard = target.getCards().get((int) (Math.random() * cardsSize-1));
+            target.getCards().remove(selectCard);
+        } else {
+            // Player has active cards and cards on hand
+            while (typeOfDeck < 1 || typeOfDeck > 2) {
+                typeOfDeck = ZKlavesnice.readInt("1. Cards on hand\n2. Cards on table");
+                if (typeOfDeck < 1 || typeOfDeck > 2) {
+                    System.out.println(" !!! Try again! !!!");
+                }
+            }
+
+            if (typeOfDeck == 1) {
+                selectCard = target.getCards().get((int) (Math.random() * cardsSize-1));
+                target.getCards().remove(selectCard);
+            } else {
+                selectCard = target.getActiveCards().get((int) (Math.random() * activeCardsSize-1));
+                target.getActiveCards().remove(selectCard);
             }
         }
 
         cardDeck.trash.add(this);
         player.removeCard(this);
+        cardDeck.trash.add(selectCard);
 
-        int cardDeckSize;
-        Card cardToRemove;
-        if (typeOfDeck == 1) {
-            cardDeckSize = target.getCards().size();
-            if (cardDeckSize == 0) {
-                return;
-            } else {
-                cardToRemove = target.getCards().get((int) (Math.random() * cardDeckSize-1));
-                target.removeCard(cardToRemove);
-            }
-        } else {
-            cardDeckSize = target.getActiveCards().size();
-            if (cardDeckSize == 0) {
-                return;
-            } else {
-                cardToRemove = target.getActiveCards().get((int) (Math.random() * cardDeckSize-1));
-                target.removeActiveCard(cardToRemove);
-            }
-        }
-        cardDeck.trash.add(cardToRemove);
-
-        System.out.println("--- " + target.getName() + " lost card " + cardToRemove.getName() + " ---");
+        System.out.println("--- " + target.getName() + " lost card " + selectCard.getName() + " ---");
     }
 }
